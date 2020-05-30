6c4824362c9bfc51bb851cc616e24f49bcd3baaf
Debidutta Rath <debiduttarath1515@gmail.com>
2020-05-30 14:15:56 +0530

    Updated by debi on 30.05 at 14:20

--- app/src/main/java/com/example/mednowpartner/ConfirmReceipt.java
+++ app/src/main/java/com/example/mednowpartner/ConfirmReceipt.java
@@ -1,15 +1,24 @@
 package com.example.mednowpartner;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mednowpartner.model.Medicine;
+import com.example.mednowpartner.model.Order;
 import com.example.mednowpartner.model.Partner;
 import com.example.mednowpartner.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
@@ -19,9 +28,12 @@ public class ConfirmReceipt extends AppCompatActivity {
     String orderId;
     User customer;
     Partner partner;
+    Order order;
 
     TextView textViewCustomerName,textViewCustomerAddress,textViewPharmacyName,textViewPharmacyAddress,textViewOrderDateTime,textViewCost;
     RecyclerView recyclerView;
+
+    DatabaseReference databaseReferenceUser,databaseReferencePartner,databaseReferenceOrder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +47,46 @@ public class ConfirmReceipt extends AppCompatActivity {
         textViewOrderDateTime = findViewById(R.id.confirm_receipt_text_view_order_date_time);
         textViewCost = findViewById(R.id.confirm_receipt_text_view_cost);
 
+        orderId = getIntent().getStringExtra("orderId");
+        databaseReferenceOrder = FirebaseDatabase.getInstance().getReference("Orders").child(orderId);
+        databaseReferencePartner = FirebaseDatabase.getInstance().getReference("Partners");
+        databaseReferenceUser = FirebaseDatabase.getInstance().getReference("Users");
+        databaseReferenceOrder.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                order = dataSnapshot.getValue(Order.class);
+                databaseReferenceOrder.removeEventListener(this);
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ConfirmReceipt.this,databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        databaseReferenceUser.child(order.getCustomerId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                customer = dataSnapshot.getValue(User.class);
+                databaseReferenceUser.child(order.getCustomerId()).removeEventListener(this);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ConfirmReceipt.this,databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
+        databaseReferencePartner.child(order.getPartnerId()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                partner = dataSnapshot.getValue(Partner.class);
+                databaseReferenceUser.child(order.getPartnerId()).removeEventListener(this);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(ConfirmReceipt.this,databaseError.getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void sendReceiptBtn(View view) {
